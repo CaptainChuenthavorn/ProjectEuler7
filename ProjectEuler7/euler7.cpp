f309f09d259107376a1cbe34207f8e902b480aed
mymint01 <captainchuenthavorn@gmail.com>
2020-08-23 21:21:08 +0700

    nearly

--- ProjectEuler7/euler7.cpp
+++ ProjectEuler7/euler7.cpp
@@ -1,34 +1,31 @@
 #include<stdio.h>
 int main()
 {
-	int n, i, j, flag, y;
+	int i, j, flag, y;
+	long long int n;
 	int prime[100];
-	
+	n = 10000000;
 	y = 0;
 	//scanf("%d",&n)
-	for (i = 2;i < 1000;i++) {
+	for (i = 2;i < n;i++) {
 		flag = 0;
-		for (j = 2;j < 500 ;j++)
+		for (j = 2;j < n/2 ;j++)
 		{
-			if ((i-1) % j == 0 ) {
+			if (i % j == 0 && j != i) {
 				flag = 1;
 				break;
 			}
 			
 		}
-		printf("\n%d / %d = %d",i,j, flag);
+		//printf("\n%d / %d = %d",i,j, flag);
 		if (flag == 0)
 		{
 			y++;
 			prime[y] = i;
 		}
 		
-		//printf("\nprime[%d]  : %d", i, prime[y]);
+		printf("\nprime[%d]  : %d", y, prime[200]);
 	}
-
-
-
-
 
 	return 0;
 }
